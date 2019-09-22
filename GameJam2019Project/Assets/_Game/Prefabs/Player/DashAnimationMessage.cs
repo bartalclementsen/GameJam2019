c15fddb7d6f8bb4617430a6dbe898db132ec230c
Rói Holgarsson <roiholgarsson@live.com>
2019-09-22 13:25:39 +0100

    Added slapped Particles in brown color

---
+++ GameJam2019Project/Assets/_Game/Prefabs/Player/DashAnimationMessage.cs
@@ -0,0 +1,14 @@
+﻿using Core.Mediators;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAnimationMessage : Message
+{
+    public int PlayerNumber { get; set; }
+
+    public DashAnimationMessage(object sender, int playerNumber) : base(sender)
+    {
+        PlayerNumber = playerNumber;
+    }
+}
---
+++ GameJam2019Project/Assets/_Game/Prefabs/Player/WasSlappedMessage.cs
@@ -0,0 +1,14 @@
+﻿using Core.Mediators;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasSlappedMessage : Message
+{
+    public int PlayerNumber { get; set; }
+
+    public WasSlappedMessage(object sender, int playerNumber) : base(sender)
+    {
+        PlayerNumber = playerNumber;
+    }
+}
