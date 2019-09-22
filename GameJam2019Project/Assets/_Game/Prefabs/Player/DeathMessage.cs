81b7b9a911cb8c64fdf7f233e907e8ea4717fd47
Rói Holgarsson <roiholgarsson@live.com>
2019-09-22 16:06:52 +0100

    Added missing files for final build

---
+++ GameJam2019Project/Assets/_Game/Prefabs/Player/DeathMessage.cs
@@ -0,0 +1,14 @@
+﻿using Core.Mediators;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMessage : Message
+{
+    public int PlayerNumber { get; set; }
+
+    public DeathMessage(object sender, int playerNumber) : base(sender)
+    {
+        PlayerNumber = playerNumber;
+    }
+}
---
+++ GameJam2019Project/Assets/_Game/Scenes/SoundHandler.cs
@@ -0,0 +1,73 @@
+﻿using Core.Mediators;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    private IMessenger _messenger;
+    private ISubscriptionToken _slappedToken;
+    private ISubscriptionToken _missToken;
+    private ISubscriptionToken _jumpToken;
+    private ISubscriptionToken _dashToken;
+    private ISubscriptionToken _deathToken;
+
+
+    [SerializeField]
+    private AudioSource _slappedSource;
+    [SerializeField]
+    private AudioSource _missSource;
+    [SerializeField]
+    private AudioSource _jumpSource;
+    [SerializeField]
+    private AudioSource _dashSource;
+    [SerializeField]
+    private AudioSource _deathSource;
+
+    [SerializeField]
+    private List<AudioClip> _jumpSounds;
+
+    void Start()
+    {
+        _messenger = Game.Container.Resolve<IMessenger>();
+
+        _slappedToken = _messenger?.Subscribe<WasSlappedMessage>((message) =>
+        {
+            _slappedSource?.PlayOneShot(_slappedSource.clip);
+        });
+
+        _missToken = _messenger?.Subscribe<SlapMessage>((message) =>
+        {
+            _missSource?.PlayOneShot(_missSource.clip);
+        });
+
+        _jumpToken = _messenger?.Subscribe<JumpMessage>((message) =>
+        {
+            _jumpSource?.PlayOneShot(_jumpSounds[message.PlayerNumber % _jumpSounds.Count]);
+        });
+
+        _dashToken = _messenger?.Subscribe<DashAnimationMessage>((message) =>
+        {
+            _dashSource?.PlayOneShot(_dashSource.clip);
+        });
+
+        _deathToken = _messenger?.Subscribe<DeathMessage>((message) =>
+        {
+            _deathSource?.PlayOneShot(_deathSource.clip);
+        });
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        _slappedToken?.Dispose();
+        _missToken?.Dispose();
+        _jumpToken?.Dispose();
+        _dashToken?.Dispose();
+        _deathToken?.Dispose();
+    }
+}
